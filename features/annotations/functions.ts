4eb4013d1b1a6900d392830df296390727fee62c
plgrazon <plgrazon@gmail.com>
2023-10-04 18:14:04 -0400

    ts - Add functions lesson

---
+++ features/annotations/functions.ts
@@ -0,0 +1,32 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+// type inference works with return values
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = (a: number, b: number): number => {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+  // return null / undefined <- works
+}
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+}
+
+
+const throwErrorAgain = (message: string): string => {
+  if (!message) throw new Error(message);
+
+  return message;
+}

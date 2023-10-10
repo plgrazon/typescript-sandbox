8b08855727eccc0ae66ef12dcea1491dba0acff7
plgrazon <plgrazon@gmail.com>
2023-10-10 10:55:45 -0400

    ts - Learn about modifiers

--- features/classes.ts
+++ features/classes.ts
@@ -1,20 +1,55 @@
 class Vehicle {
-  drive(): void {
+  color: string;
+  // or
+  // color: string = 'red';
+
+  constructor(color: string) {
+    this.color = color;
+  }
+
+  // another syntax that is shorter
+  // no need to initialize the property
+  // constructor([modifier] color: string) {}
+
+  public drive(): void {
     console.log('vroom vroom');
   }
 
-  honk(): void {
-    console.log('beep')
+  public honk(): void {
+    console.log('beep');
+  }
+
+  protected reverse(): void {
+    console.log('moving back');
   }
 }
 
 class Car extends Vehicle {
   // override 
-  drive(): void {
+  // changing the access modifier wont work here because the parent modifier
+  // is different
+  // private drive(): void { 
+  //   console.log('zoom zoom')
+  // }
+
+  // correct way:
+  public drive(): void {
     console.log('zoom zoom')
+  }
+
+  startDriving(): void {
+    this.drive();
+  }
+
+  startReversing(): void {
+    this.reverse();
   }
 }
 
-const vehicle = new Car();
-vehicle.drive();
+const vehicle = new Car('orange');
+// vehicle.drive(); // this wont work anymore because drive is now private
 vehicle.honk();
+// correct way:
+vehicle.startDriving();
+vehicle.startReversing();
+console.log(vehicle.color);

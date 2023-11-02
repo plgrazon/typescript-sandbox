3169edfdf8b3233e8edbbf4e920b37a378103648
plgrazon <plgrazon@gmail.com>
2023-11-02 15:10:28 -0400

    ts - Add new modules

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,3 +1,10 @@
-console.log('hello world');
 // to start working use parcel
 // npx parcel [entry file]
+
+import { Company } from './Company';
+import { User } from './User';
+
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company)

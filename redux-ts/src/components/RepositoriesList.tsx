458f92cd3677df7587f848d9ec47fa6c83bebe7a
plgrazon <plgrazon@gmail.com>
2024-06-13 18:57:54 -0400

    ts - update project

---
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -0,0 +1,10 @@
+export const RepositoriesList: React.FC = () => {
+  return (
+    <div>
+      <form>
+        <input></input>
+        <button>Search</button>
+      </form>
+    </div>
+  );
+};

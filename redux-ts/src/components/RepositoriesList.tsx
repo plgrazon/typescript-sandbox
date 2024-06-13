069d74625ce5221a6baf078179877ebd36824198
plgrazon <plgrazon@gmail.com>
2024-06-13 19:32:35 -0400

    ts - add state

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useActions } from "../hooks/useActions";
+
 export const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState<string>("");
+  // const dispatch = useDispatch();
+  const { searchRepositories } = useActions();
+  const { loading, error, data } = useSelector((state) => state.repositories);
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // dispatch(actionCreators.searchRepositories(term) as any);
+    searchRepositories(term);
+  };
+
   return (
     <div>
-      <form>
-        <input></input>
+      <form onSubmit={onSubmit}>
+        <input onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>

5a6e636bb78c616618f6895b33910bfe31baf666
plgrazon <plgrazon@gmail.com>
2024-06-14 07:03:25 -0400

    ts - map

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -24,6 +24,9 @@ export const RepositoriesList: React.FC = () => {
         <input onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <p>{name}</p>)}
     </div>
   );
 };
